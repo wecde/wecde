891c7fa1ff82ad97ee3b9ac31c7c83d55caf28a6
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2021-09-12 21:05:06 +0700

    (update) webserver template & eruda

--- src/boot/webserver.ts
+++ src/boot/webserver.ts
@@ -4,33 +4,20 @@ import fs from "modules/fs";
 import { extname, join } from "path-cross";
 import { boot } from "quasar/wrappers";
 
-import codeEruda from "!raw-loader!eruda2/eruda.js";
-
 boot(({ store }) => {
-  const base64CodeEruda = [
-    "data:application/javascript;base64,",
-    btoa(
-      unescape(
-        encodeURIComponent(
-          codeEruda +
-            `;
-          eruda.init({
-      useShadowDom: true,
-      autoScale: true,
-
-      defaults: {
-        displaySize: 50,
-        transparency: 0.9,
-        theme: "Monokai Pro",
-      },
-    });`
-        )
-      )
-    ),
-  ].join("");
   const eruda2 = document.createElement("script");
   eruda2.setAttribute("type", "text/javascript");
-  eruda2.setAttribute("src", base64CodeEruda);
+  eruda2.setAttribute(
+    "src",
+    `data:application/javascript;base64,${btoa(
+      unescape(
+        encodeURIComponent(
+          require("!raw-loader!eruda2/eruda.js") +
+            ';eruda.init({useShadowDom:true,autoScale:true,defaults:{displaySize:50,transparency:0.9,theme:"Monokai Pro"}});'
+        )
+      )
+    )}`
+  );
   eruda2.setAttribute("id", "eruda-loader");
 
   function addEruda(html: string): string {
@@ -65,65 +52,61 @@ boot(({ store }) => {
         //   webserverConfigJSON = {};
         // }
 
-        if (project) {
-          const path = join(`projects/${project as string}`, data.path.slice(1));
+        try {
+          if (!project) {
+            // eslint-disable-next-line functional/no-throw-statement
+            throw new Error("NOT_FOUND");
+          }
+
+          const path = join(
+            `projects/${project as string}`,
+            data.path.slice(1)
+          );
 
           const thisStat = await fs.stat(path);
 
-          try {
-            if (thisStat) {
-              // eslint-disable-next-line functional/no-let
-              let pathToFile = path;
+          if (thisStat) {
+            // eslint-disable-next-line functional/no-let
+            let pathToFile = path;
 
-              if (thisStat.isDirectory()) {
-                pathToFile = join(path, "index.html");
-              }
+            if (thisStat.isDirectory()) {
+              pathToFile = join(path, "index.html");
+            }
 
-              if (await fs.stat(pathToFile)) {
-                if (/^\.html?$/.test(extname(pathToFile))) {
-                  await WebServer.sendResponse(data.requestId, {
-                    status: 200,
-                    body: addEruda(await fs.readFile(pathToFile, "utf8")),
-                    headers: {
-                      "Content-Type": "text/html",
-                    },
-                  });
-                } else {
-                  await WebServer.sendResponse(data.requestId, {
-                    status: 201,
-                    path: await fs.getUri(pathToFile),
-                    headers: {},
-                  });
-                }
+            if (await fs.stat(pathToFile)) {
+              if (/^\.html?$/.test(extname(pathToFile))) {
+                await WebServer.sendResponse(data.requestId, {
+                  status: 200,
+                  body: data.headers
+                    .split("\n")
+                    .find((item) => item.startsWith("HTTP_X_REQUESTED_WITH: "))
+                    ?.includes("xmlhttprequest")
+                    ? await fs.readFile(pathToFile, "utf8")
+                    : addEruda(await fs.readFile(pathToFile, "utf8")),
+                  headers: {
+                    "Content-Type": "text/html",
+                  },
+                });
               } else {
-                // eslint-disable-next-line functional/no-throw-statement
-                throw new Error("NOT_FOUND");
+                await WebServer.sendResponse(data.requestId, {
+                  status: 201,
+                  path: await fs.getUri(pathToFile),
+                  headers: {},
+                });
               }
             } else {
               // eslint-disable-next-line functional/no-throw-statement
               throw new Error("NOT_FOUND");
             }
-          } catch {
-            await WebServer.sendResponse(data.requestId, {
-              status: 404,
-              body: `
-          <h1>404 Not Found</h1>
-        `,
-              headers: {
-                "Content-Type": "text/html",
-              },
-            });
+          } else {
+            // eslint-disable-next-line functional/no-throw-statement
+            throw new Error("NOT_FOUND");
           }
-        } else {
+        } catch {
           await WebServer.sendResponse(data.requestId, {
             status: 404,
-            body: `
-        <h1>404 Not Found</h1>
-        <h3>Please select project Web then use WebServer.</h3>
-      `,
-            headers: {
-              "Content-Type": "text/html",
-            },
+            path: require("src/webserver/404.html"),
+            headers: {},
           });
         }
       }
