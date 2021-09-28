ac689aeb7daf02885672c8644b991221704bbe9a
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-09-28 12:13:07 +0100

    (update) font app-title

--- src/components/Preload.vue
+++ src/components/Preload.vue
@@ -104,16 +104,7 @@ void init();
 </script>
 
 <style lang="scss" scoped>
-@font-face {
-  font-family: "Orbitron";
-  font-style: normal;
-  font-weight: 400;
-  font-display: swap;
-  src: url(~assets/fonts/Orbitron.woff2) format("woff2");
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
-    U+FEFF, U+FFFD;
-}
+@import "scss/font-Orbitron.scss";
 
 .app--name {
   font-family: Orbitron;
